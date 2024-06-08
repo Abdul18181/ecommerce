41217f58dfff2f1d7161131570b66bd5a3e2f7d1
Abdul Wahab BK <wahabk34537@gmail.com>
2024-06-08 20:06:54 +0500

    changed

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ let header  = document.querySelector('header')
 let nav1  = document.querySelector('.nav1')
 // let category = document.querySelector('.category a')
 window.onscroll = ()=>{
-    if(window.scrollY>250){
+    if(window.scrollY>50){
         header.classList.add('active')
         // category.classList.add('active-cat')
         nav1.classList.add('active-nav')
-    }else if(window.scrollY<250){
+    }else if(window.scrollY<50){
         header.classList.remove('active')
         nav1.classList.remove('active-nav')
         // category.classList.remove('active-cat')
